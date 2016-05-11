b63a3e1a1e14b087ca4db983b5a56969ffdd6d44
Fernando Seabra Chirigati <fchirigati@nyu.edu>
2016-05-11 15:49:12 -0400

    Fixing path to Python.

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
-#!../bin/python
+#!../.virtualenvs/sdp_curricula/bin/python
 import os
 import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sdp_curricula.settings")
-    sys.path.append('../lib/python/2.7/site-packages')
+    sys.path.append('../.virtualenvs/sdp_curricula/lib/python/2.7/site-packages')
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
